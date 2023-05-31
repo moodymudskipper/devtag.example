8935fe552cb95e7a5346d56f70a229f1962111a0
Antoine Fabri <antoine.fabri@gmail.com>
2023-05-31 22:35:40 +0200

    Initial commit

---
+++ R/funs.R
@@ -0,0 +1,31 @@
+#' @import devtag
+NULL
+
+#' add
+#'
+#' @param x numeric
+#' @param y numeric
+#'
+#' @export
+add <- function(x, y) {
+  x + y
+}
+
+#' subtract
+#'
+#' @param x numeric
+#' @param y numeric
+#' @dev
+subtract <- function(x, y) {
+  x - y
+}
+
+#' multiply
+#'
+#' @param x numeric
+#' @param y numeric
+#' @noRd
+multiply <- function(x, y) {
+  x * y
+}
+
